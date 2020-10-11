0bdb080f91e03b6c1219389dc35d393ea0f313bf
David Rosenberg <dmr@davidrosenberg.me>
2020-10-11 19:38:43 -0400

    cleanup dummy data maker script

--- dummy_data_maker.py
+++ dummy_data_maker.py
@@ -15,16 +15,40 @@ and manipulate
 import random
 
 names = [
-    "Kara Thrace", "Nancie Hogg", "Jeri Sherrard", "Moises Newsom", "Gearldine Foxworth",
-    "Serina Lacomb", "Obdulia Delorenzo", "Meghan Beatson", "Lavada Golay", "Marnie Frith",
-    "Trena Cressey", "Hermelinda Mullikin", "Cherly Caso", "Ola Torres",
-    "Jacalyn Crews", "Piper Rolan-Adeyemi", "Lezlie Judd", "Marchelle Goode",
-    "Evonne Granda", "Arlie Ditzler", "Carl Silverberg", "Sharolyn Erb",
-    "Marg Brazelton", "Ericka Billups", "Claudia Philips", "Thomas Butterworth",
-    "Dawna Cost", "Willodean Delafuente", "Youlanda Zajicek", "Gwenn Arnold"]
+    "Kara Thrace",
+    "Nancie Hogg",
+    "Jeri Sherrard",
+    "Moises Newsom",
+    "Gearldine Foxworth",
+    "Serina Lacomb",
+    "Obdulia Delorenzo",
+    "Meghan Beatson",
+    "Lavada Golay",
+    "Marnie Frith",
+    "Trena Cressey",
+    "Hermelinda Mullikin",
+    "Cherly Caso",
+    "Ola Torres",
+    "Jacalyn Crews",
+    "Piper Rolan-Adeyemi",
+    "Lezlie Judd",
+    "Marchelle Goode",
+    "Evonne Granda",
+    "Arlie Ditzler",
+    "Frances Steed",
+    "Emerita McMillian",
+    "Marg Brazelton",
+    "Ericka Billups",
+    "Claudia Philips",
+    "Thomas Butterworth",
+    "Dawna Cost",
+    "Willodean Delafuente",
+    "Youlanda Zajicek",
+    "Gwenn Arnold",
+]
 
 
-def build_entry(oncall = False):
+def build_entry(oncall=False):
     """build a randomly timed db entry from the names set above
 
     :function: TODO
@@ -35,17 +59,25 @@ def build_entry(oncall = False):
     names.remove(entryname)
     entrycallback = "x" + str(random.randrange(1000, 9999))
     entryservice = str(random.randrange(1, 40))
-    entryaddtime = "current_timestamp + interval '%s minutes' + interval '%s seconds'" % (str(random.randrange(1, 60)), str(random.randrange(1, 60)))
+    entryaddtime = (
+        "current_timestamp + interval '%s minutes' + interval '%s seconds'"
+        % (str(random.randrange(1, 60)), str(random.randrange(1, 60)))
+    )
     entryoncall = "FALSE"
     if oncall:
         entryoncall = "TRUE"
         entryaddtime = entryaddtime + " + interval '1 hour'"
-    querystring = """INSERT INTO signout (intern_name, intern_callback, service, oncall, addtime) VALUES ('%s', '%s', %s, %s, %s);""" % (entryname, entrycallback, entryservice, entryoncall, entryaddtime)
+    querystring = (
+        """
+        INSERT INTO signout (intern_name, intern_callback, service, oncall, addtime) 
+        VALUES (%s, %s, %s, %s, %s);""",
+        (entryname, entrycallback, entryservice, entryoncall, entryaddtime),
+    )
     print(querystring)
+
 
 if __name__ == "__main__":
     for x in range(13):
         build_entry()
     for x in range(7):
         build_entry(True)
-
