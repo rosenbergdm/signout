bd3538a41f092802edb55370b7334f70a37fd9fa
David Rosenberg <dmr@davidrosenberg.me>
2021-03-19 14:20:16 -0400

    license change

--- src/signout/helpers.py
+++ src/signout/helpers.py
@@ -1,11 +1,23 @@
-#! /usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # vim:enc=utf-8
 #
-# Copyright © 2020-2021 David M. Rosenberg <dmr@davidrosenberg.me>
-# Last updated Sat Feb 27 00:13:00 EST 2021
+#    This file is part of signout.py the house staff web-based signout
+#    manager for MSKCC.
+#    Copyright © 2020-2021 David M. Rosenberg <dmr@davidrosenberg.me>
 #
-# Distributed under terms of the MIT license.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 signout/helpers.py
--- src/signout/static/signout.js
+++ src/signout/static/signout.js
@@ -1,11 +1,26 @@
-// js
-// static/signout.js
+// /usr/bin/env node
 // -*- coding: utf-8 -*-
-// vim:fenc=utf-8
+// vim:enc=utf-8
 //
-// Copyright © 2020-2021 David M. Rosenberg <dmr@davidrosenberg.me>
+//    This file is part of signout.py the house staff web-based signout
+//    manager for MSKCC.
+//    Copyright © 2020-2021 David M. Rosenberg <dmr@davidrosenberg.me>
 //
-// Distributed under terms of the MIT license.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// static/signout.js
+// MSKCC signout.py javascript library
 
 //{{{ Global variables and initialization
 
